7e96e7cd40a21f5844298a3d505c346e195fcb1b
ティン・ルーフ <tinruufu@gmail.com>
2016-07-14 23:10:55 +0100

    six

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if version_info.major != 3 or version_info.minor < 5:
 
 setup(
     name='friction',
-    version='0.5',
+    version='0.6',
     description=(
         'a browser-based gallery viewer tailored for viewing large '
         'collections of pornographic manga'
