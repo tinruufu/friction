a8c706af46954d732cdb792fb37b6ca9134db124
ティン・ルーフ <tinruufu@gmail.com>
2016-07-23 20:29:06 +0100

    eight

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if version_info.major != 3 or version_info.minor < 5:
 
 setup(
     name='friction',
-    version='0.7',
+    version='0.8',
     description=(
         'a browser-based gallery viewer tailored for viewing large '
         'collections of pornographic manga'
